fd1364d68ab3905c08bd7272f65bea94582925ea
Pratham Sahu <92453722+Prathamsahu52@users.noreply.github.com>
2022-12-16 23:50:02 +0530

    Add files via upload

--- pages/AcadQualCard.tsx
+++ pages/AcadQualCard.tsx
@@ -1,7 +1,45 @@
 import React from 'react'
-import {  Button, Card, CssBaseline, Popover, TextField } from '@mui/material'
+import {  Button, Card, CssBaseline, Grid, Popover, Stack, TextField } from '@mui/material'
 
 
-const AcadCard=() => { 
-    
+export default function AcadCard(){
+
+    return(
+        <Card elevation={5}
+            sx={{
+              padding: 5,
+              width: {
+                md: "100%",
+                xs: "100%",
+              },
+            }}>
+
+                <Stack spacing={2}>
+                    <Stack direction="row" spacing={2}>
+
+                        
+                            <TextField  label="Start Year" variant="outlined">
+                            </TextField>
+                        
+                            
+                        
+                            <TextField  label="End Year" variant="outlined">
+                            </TextField>
+                        
+                        <TextField label="CPI/Percentage" variant="outlined">
+                        </TextField>
+
+
+                        
+
+                    </Stack>
+
+                    <TextField size="medium" label="School/Institute" variant="outlined">
+                    </TextField> 
+                    
+                </Stack>
+            
+          
+          </Card>
+    )
 }
---
+++ pages/SchalasticAchievCard.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {  Button, Card, CssBaseline, Grid, Popover, Stack, TextField } from '@mui/material'
+
+
+export default function ScholasticCard(){
+
+
+    return(
+        <Card elevation={5}
+        sx={{
+          padding: 5,
+          width: {
+            md: "100%",
+            xs: "100%",
+          },
+        }}>
+            <Stack spacing={2}>
+                
+            </Stack>
+            
+        
+      
+      </Card>
+    )
+    
+}
